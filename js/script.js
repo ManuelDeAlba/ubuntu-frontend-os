ff0cafc1111ea058d8dd752d11efc15632f0ba3c
Manuel De Alba <ferman5903@gmail.com>
2024-03-16 12:34:15 -0600

    Resize the windows

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@ const terminal = document.querySelector(".terminal");
 let dragInfo = {
     element: null,
     isDragging: false,
+    edge: null,
+    isResizing: false,
     x: undefined,
     y: undefined,
+    w: undefined,
+    h: undefined,
     relativeX: undefined,
     relativeY: undefined
 }
@@ -44,12 +48,21 @@ actionOnClick(".bar-icon", (e, target) => {
 })
 
 actionOnClick(".window", (e) => {
+    // Check the edges
+    const edge = e.target;
     const clickedWindow = e.target.closest(".window");
 
     // Bring the window to the front
     const windows = document.querySelectorAll(".window");
     windows.forEach(w => w.classList.remove("active"));
     clickedWindow.classList.add("active");
+
+    dragInfo.x = e.clientX;
+    dragInfo.y = e.clientY;
+    dragInfo.w = clickedWindow.offsetWidth;
+    dragInfo.h = clickedWindow.offsetHeight;
+    dragInfo.isResizing = true;
+    dragInfo.edge = edge;
 })
 
 actionOnClick(".top-bar", (e, target) => {
@@ -69,8 +82,13 @@ actionOnClick(".minimize", (e) => {
 actionOnClick(".maximize", (e) => {
     const clickedWindow = e.target.closest(".window");
     clickedWindow.classList.toggle("maximized");
-    clickedWindow.style.top = "";
-    clickedWindow.style.left = "";
+
+    if(clickedWindow.matches(".maximized")){
+        clickedWindow.style.top = "";
+        clickedWindow.style.left = "";
+        clickedWindow.style.width = "";
+        clickedWindow.style.height = "";
+    }
 })
 
 actionOnClick(".close", (e) => {
@@ -136,10 +154,49 @@ window.addEventListener("mousemove", e => {
     const x = e.clientX;
     const y = e.clientY;
 
+    // Draw the window
     if (dragInfo.isDragging && dragInfo.element) {
         const currentWindow = dragInfo.element.closest(".window");
         currentWindow.style.left = `${clamp(x - dragInfo.relativeX, -currentWindow.scrollWidth/2, innerWidth-currentWindow.scrollWidth/2)}px`;
         currentWindow.style.top = `${clamp(y - dragInfo.relativeY, 0, window.innerHeight-currentWindow.scrollHeight/2)}px`;
+    }
+
+    // Resize the window
+    if(dragInfo.isResizing && dragInfo.edge){
+        const currentWindow = dragInfo.edge.closest(".window");
+        
+        const minWidth = 250;
+        const minHeight = 250;
+
+        if(dragInfo.edge.matches(".left")){
+            let dx = -(x - dragInfo.x); // Difference between the start x and the current x
+            if(dragInfo.w + dx < minWidth) return; // If the new width is less than the minimum, don't move the window
+
+            const newWidth = clamp(dragInfo.w + dx, minWidth, window.innerWidth - currentWindow.offsetLeft);
+            const newLeft = clamp(x, 0, window.innerWidth - currentWindow.offsetWidth);
+
+            currentWindow.style.width = `${newWidth}px`;
+            currentWindow.style.left = `${newLeft}px`;
+        }
+        if(dragInfo.edge.matches(".right")){
+            const newWidth = clamp(x - currentWindow.offsetLeft, minWidth, window.innerWidth);
+            currentWindow.style.width = `${newWidth}px`;
+        }
+        if(dragInfo.edge.matches(".top")){
+            let dy = -(y - dragInfo.y); // Difference between the start x and the current x
+            if(dragInfo.h + dy < minHeight) return; // If the new height is less than the minimum, don't move the window
+
+            const newHeight = clamp(dragInfo.h + dy, minWidth, window.innerHeight - currentWindow.offsetTop);
+            const newTop = clamp(y, 0, window.innerHeight - currentWindow.offsetHeight);
+
+            currentWindow.style.height = `${newHeight}px`;
+            currentWindow.style.top = `${newTop}px`;
+        }
+        if(dragInfo.edge.matches(".bottom")){
+            const newHeight = clamp(y - currentWindow.offsetTop, minHeight, window.innerHeight);
+            currentWindow.style.height = `${newHeight}px`;
+        }
+    
     }
 })
 
@@ -147,8 +204,12 @@ window.addEventListener('mouseup', e => {
     dragInfo = {
         element: null,
         isDragging: false,
+        edge: null,
+        isResizing: false,
         x: undefined,
         y: undefined,
+        w: undefined,
+        h: undefined,
         relativeX: undefined,
         relativeY: undefined
     }
